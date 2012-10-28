4d512c5b2b78cc98c0257cc76b92c190e05a01f8
HectorJDK <hector.hdp90@gmail.com>
2012-10-27 20:32:06 -0500

    Primer version clases

---
+++ Models/Usuario.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @access public
+ * @author hector
+ */
+class Usuario {
+	/**
+	 * @AttributeType String
+	 */
+	private $_usuario;
+	/**
+	 * @AttributeType String
+	 */
+	private $_password;
+
+	/**
+	 * @access public
+	 * @param aFile
+	 * @return void
+	 * 
+	 * @ReturnType void
+	 */
+	public function SubirDocumento($aFile) {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 * @param aFile
+	 * @return void
+	 * 
+	 * @ReturnType void
+	 */
+	public function EliminarDocumento($aFile) {
+		// Not yet implemented
+	}
+}
+?>
